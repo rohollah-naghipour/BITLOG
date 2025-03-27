9e3c3d66d45b510b59ab6cd3b9bc5305c55ec7dc
rohy <rohollahnaghipour41@gmail.com>
2025-03-27 15:49:35 +0330

    add template login and register

--- products/urls.py
+++ products/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path("contact/",views.ContactPage.as_view(), name = 'contact'),
     path("about/", views.AboutPage.as_view(), name='about'),
     path("category-summary/", views.CategoryPage.as_view(), name = 'category-summary'),
-    path('category/<str:cat>', views.category, name = 'category'),
-    path('single-article/<str:cat>', views.SingleArticle, name = 'single-article')
+    path('category/<str:cat>', views.CategoryView.as_view(), name = 'category'),
+    path('single-article/<str:cat>', views.SingleArticle.as_view(), name = 'single-article'),
+    path('login/', views.Login.as_view(), name = 'login'),
+    path('register/', views.Register.as_view(), name = 'register'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,98 +1,61 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-from products.models import *
 from django.contrib import messages
 
+import re as r
 
-class ContactPage(TemplateView):
-    template_name = "page-contact.html"
+from products.models import *
 
-class AboutPage(TemplateView):
-    template_name = 'page-about.html'
 
-#def category_summary(request):
-    """Display a list of all Categories"""
-    #categories = Category.objects.all()
-    #context = {"categories":categories}
-    #return render(request, 'category-summary.html', context)
+
+
+
+
+class Login(TemplateView):
+    template_name = 'login.html'
+
+class Register(TemplateView):
+    template_name = 'register.html'
+
+
 
 class CategoryPage(TemplateView):
     def get(self, request, **kwargs):
         categories = Category.objects.all()
         return render(request, 'category-summary.html', {'categories': categories})  
-     
-
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from .models import Category, Article
-import re
-
-def category(request, cat):
-    """نمایش مقالات یک دسته‌بندی خاص"""
-    # پردازش عنوان فارسی
-    cat = re.sub(r'[-_]+', ' ', cat).strip().title()
     
-    try:
-        # جستجو با عنوان فارسی
-        category = Category.objects.get(title__iexact=cat)
-        articles = Article.objects.filter(category=category)
+class CategoryView(TemplateView):
+    def get(self, request, cat):
+        cat = r.sub(r'[-_]+', ' ', cat).strip().title()
         
-        return render(request, 'category.html', {
-            'category': category,
-            'articles': articles
-        })
+        try:
+            category = Category.objects.get(title__iexact=cat)
+            articles = Article.objects.filter(category=category)
+
+            context = {
+                'category': category,
+                'articles': articles}
         
-    except Category.DoesNotExist:
-        messages.error(request, f"دسته‌بندی '{cat}' یافت نشد")
-        return render(request, 'category-summary.html')
+            return render(request, 'category.html', context)
+            
+        except Category.DoesNotExist:
+            messages.error(request, f"دسته‌بندی '{cat}' یافت نشد")
+            
+            return render(request, 'category-summary.html')
 
+class SingleArticle(TemplateView):
+    def get(self, request, cat):
+        try:
+            cat = r.sub(r'[-_]+', ' ', cat).strip().title()
 
-def SingleArticle(request, cat):
-    try:
-        cat = re.sub(r'[-_]+', ' ', cat).strip().title()
-
-        articles = Article.objects.filter(title__iexact = cat)
-        return render(request, 'single-article.html', {'articles': articles})
-    
-    except articles.DoesNotExist:
-        return render(request, '404.html', {'message': f"دسته‌بندی '{cat}' یافت نشد"}, status=404)
-
-
-#def SingleArticle(request, cat):
-    #cat = re.sub(r'[-_]+', ' ', cat).strip().title()
-
-    #category = Category.objects.get(title__iexact=cat)
-    #print(category)
-    #articles = Article.objects.filter(category=category)
-    #print(articles)
-    
-    #return render(request, 'single-article.html', {'articles': articles})
+            articles = Article.objects.filter(title__iexact = cat)
+            return render(request, 'single-article.html', {'articles': articles})
         
- 
-  
-
-
-#def category(request, cat):
-    """Refine items to a category"""
-    # Replace hyphens with spaces
-   # cat = cat.replace('-', ' ').title()
-   # print(f"cat after .replace: {cat}")
-    
-   # try:
-       # category =  Category.objects.get(name=cat)
-       # print(category)
-       # Articles =  Article.objects.filter(category=category)
-       # print(Articles)
-       # return render(request, 'category.html', {"category": category,"Articles":Articles})
-   # except:
-       #messages.error(request, ("That category doesn't exist")) 
-       #return render(request, 'category-summary.html')
-           
-        
-        
+        except articles.DoesNotExist:
+            return render(request, '404.html', {'message': f"دسته‌بندی '{cat}' یافت نشد"}, status=404)
+                
 
 class Indexpage(TemplateView):
-    #template_name = "index.html"
     def get(self, request, **kwargs):
         
         article_data = []
@@ -108,12 +71,6 @@ class Indexpage(TemplateView):
         
         promote_data = []
         all_promote_articles = Article.objects.filter(promote=True)
-
-        #profile_name_search = Article.objects.get(profile_name=)
-        #user_avatar = User.objects.all() #(user=profile_name_search.user.pk)
-        #for p in all_promote_articles: 
-            #print(p.author.user.username, "firstname")
-        #print(user_avatar.user.last_name)
 
         for promote_article in all_promote_articles:
             promote_data.append({
@@ -133,3 +90,11 @@ class Indexpage(TemplateView):
         return render(request, 'index.html', context)
 
 
+
+class ContactPage(TemplateView):
+    template_name = "page-contact.html"
+
+
+
+class AboutPage(TemplateView):
+    template_name = 'page-about.html'
